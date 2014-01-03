848bf9582ff0431086ae5c86fd2d7f9e36cc7e44
mantal <davidlancar@hotmail.fr>
2014-01-03 11:45:37 +0100

    Marked ft_memccpy as unsafe.

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,12 +6,16 @@
 /*   By: dlancar <dlancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/27 16:18:58 by dlancar           #+#    #+#             */
-/*   Updated: 2013/11/27 16:25:27 by dlancar          ###   ########.fr       */
+/*   Updated: 2014/01/03 11:45:11 by dlancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include "libft.h"
+
+/*
+** WARNING: This function may not work.
+*/
 
 void	*ft_memccpy(void *s1, const void *s2, int c, size_t n)
 {
@@ -35,8 +39,3 @@ void	*ft_memccpy(void *s1, const void *s2, int c, size_t n)
 	return (NULL);
 }
 
-/*
-** dst[i] = src[i];
-** if (src[i] == x)
-**	return ((void*)&dst[i]);
-*/
