fb0a901541ce83a2ee5553f6b6d98e9c9731aecd
mantal <davidlancar@hotmail.fr>
2014-01-17 17:47:04 +0100

    Remove warning, remove useless null check

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,16 +6,12 @@
 /*   By: dlancar <dlancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/27 16:18:58 by dlancar           #+#    #+#             */
-/*   Updated: 2014/01/03 11:45:11 by dlancar          ###   ########.fr       */
+/*   Updated: 2014/01/17 17:46:12 by dlancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include "libft.h"
-
-/*
-** WARNING: This function may not work.
-*/
+#include <string.h>
 
 void	*ft_memccpy(void *s1, const void *s2, int c, size_t n)
 {
@@ -28,8 +24,6 @@ void	*ft_memccpy(void *s1, const void *s2, int c, size_t n)
 	src = (unsigned char*)s2;
 	x = (unsigned char)c;
 	i = 0;
-	if (!s1 || !s2 || !n)
-		return (NULL);
 	while (i < n)
 	{
 		if ((*dst++ = *src++) == x)
