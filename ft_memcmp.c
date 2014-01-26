38663a5869d4ae6cfb6eb5508acb15aa4262875e
mantal <davidlancar@hotmail.fr>
2014-01-26 16:42:33 +0100

    Removed useless NULL check.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,23 +6,18 @@
 /*   By: dlancar <dlancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/27 16:38:06 by dlancar           #+#    #+#             */
-/*   Updated: 2013/11/27 17:31:53 by dlancar          ###   ########.fr       */
+/*   Updated: 2014/01/26 16:39:32 by dlancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include "libft.h"
+#include <string.h>
+
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned int	i;
 
 	i = 0;
-	if (!s1 && !s2)
-		return (0);
-	else if (!s1 && s2)
-		return (-*((unsigned char*)s2));
-	else if (s1 && !s2)
-		return (*((unsigned char*)s1));
 	while (i < n)
 	{
 		if (((unsigned char*)s1)[i] != ((unsigned char*)s2)[i])
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dlancar <dlancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/27 16:38:06 by dlancar           #+#    #+#             */
-/*   Updated: 2013/11/27 16:55:15 by dlancar          ###   ########.fr       */
+/*   Updated: 2014/01/26 16:38:18 by dlancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *s1, const void *s2, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	if (!s1 || !s2)
-		return (s1);
 	while (i < n)
 	{
 		((char*)s1)[i] = ((char*)s2)[i];
