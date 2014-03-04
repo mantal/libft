1c5b6cb7451c7924904f7842892f893650c06630
mantal <davidlancar@hotmail.fr>
2014-03-04 12:04:49 +0100

    Fixed memory leak in ft_strtrim #1

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: dlancar <dlancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/25 09:48:45 by dlancar           #+#    #+#             */
-/*   Updated: 2014/02/05 18:28:08 by dlancar          ###   ########.fr       */
+/*   Updated: 2014/03/04 12:04:11 by dlancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,12 @@ static int	is_useless(char c)
 
 char		*ft_strtrim(const char *s)
 {
-	char	*res;
-	char	*temp;
+	char		*temp;
 
 	while (is_useless(*s))
 		s++;
-	res = ft_strdup(s);
-	if (!res)
-		return (NULL);
-	temp = ft_strchr(res, '\0');
+	temp = ft_strchr(s, '\0') - 1;
 	while (is_useless(*temp))
 		temp--;
-	return (ft_strsub(res, 0, temp - res));
+	return (ft_strsub(s, 0, temp - s + 1));
 }
