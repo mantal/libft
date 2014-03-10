e8cb2e8be9175e237fef0492bd166038c67763ba
mantal <davidlancar@hotmail.fr>
2014-03-10 15:43:38 +0100

    ft_memalloc now use ft_malloc instead of malloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: dlancar <dlancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/21 12:51:30 by dlancar           #+#    #+#             */
-/*   Updated: 2013/11/21 12:51:31 by dlancar          ###   ########.fr       */
+/*   Updated: 2014/03/10 15:43:16 by dlancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memalloc(size_t size)
 {
 	void	*ptr;
 
-	ptr = malloc(size);
+	ptr = ft_malloc(size);
 	if (!ptr)
 		return (NULL);
 	ft_memset(ptr, 0, size);
