3bb8df1ab4a4aebeed9a7ab448549e4b524ba43a
dlancar <dlancar@student.42.fr>
2016-01-09 17:08:31 +0100

    Fix ft_putstr

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,18 +6,13 @@
 /*   By: dlancar <dlancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/27 16:38:06 by dlancar           #+#    #+#             */
-/*   Updated: 2016/01/09 17:00:49 by dlancar          ###   ########.fr       */
+/*   Updated: 2016/01/09 17:08:16 by dlancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ftio.h"
-#include "libft.h"
-#include <unistd.h>
 
 void	ft_putstr(const char *s)
 {
-	if (!s)
-		write(STDOUT, "(null)", 6);
-	else
-		write(STDOUT, s, ft_strlen(s));
+	ft_putstr_fd(STDOUT, s);
 }
