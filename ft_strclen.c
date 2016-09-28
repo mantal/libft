e8f4fc56b3a04657fb4c7a31e05b253468411db7
mantal <mantal0@outlook.fr>
2016-09-28 18:19:40 +0200

    Improve ft_strclen code

--- ft_strclen.c
+++ ft_strclen.c
@@ -6,7 +6,7 @@
 /*   By: dlancar <dlancar@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/28 09:31:47 by dlancar           #+#    #+#             */
-/*   Updated: 2016/01/12 15:23:16 by dlancar          ###   ########.fr       */
+/*   Updated: 2016/04/07 11:54:52 by dlancar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@
 
 size_t	ft_strclen(const char *s, char c)
 {
-	size_t	res;
+	size_t	i;
 
-	res = 0;
-	if (!s)
-		return (res);
-	while (*s && *s != c)
-	{
-		res++;
-		s++;
-	}
-	return (res);
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
 }
